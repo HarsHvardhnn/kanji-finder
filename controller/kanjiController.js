50d2566f443a5d2a3b97c3f48725e28886f58c2e
Harsh <harshvchawla997@gmail.com>
2024-06-27 14:28:47 +0530

    grades

--- controller/kanjiController.js
+++ controller/kanjiController.js
@@ -1,6 +1,6 @@
 const jishoApi = require('unofficial-jisho-api');
 const jisho = new jishoApi();
-
+const axios = require('axios')
 exports.findByKanji = async (req,res) =>{
 
     const {kanji} = req.params ;
@@ -19,3 +19,16 @@ exports.findByKanji = async (req,res) =>{
 
 
 }
+
+
+exports.listByGrade = async (req, res) => {
+  const { grade } = req.params;
+
+  try {
+    const response = await axios.get(`https://kanjiapi.dev/v1/kanji/grade-${grade}`);
+    res.json(response.data); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching the data' }); 
+  }
+};
--- routes/route.js
+++ routes/route.js
@@ -3,4 +3,5 @@ const kanjiController = require('../controller/kanjiController');
 const router = express.Router();
 
 router.get('/find-kanji/:kanji' , kanjiController.findByKanji);
+router.get('/find-grade/:grade',kanjiController.listByGrade);
 module.exports =router;
